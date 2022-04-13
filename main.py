277192005d32ca9e22e25e651b963cc63c6b50bb
George Khaburzaniya <george.khaburzaniya@gmail.com>
2022-04-13 11:25:46 -0700

    Addition, parsing and formatting.

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from fractions import Fraction
+
 
 def main():
     print("What fractions do you wish to compute? For Example: '1/2 * 3_3/4'")
@@ -5,34 +7,48 @@ def main():
     print(compute(equation))
 
 
-class Fraction:
-    def __init__(self, whole, numerator, denominator):
-        self.whole = whole
-        self.numerator = numerator
-        self.denominator = denominator
-
-    @classmethod
-    def from_string(cls, num):
-        if '/' not in num:
-            return cls.__init__(int(num), None, None)
-        else:
-            numerator, denominator = num.split('/')
-            return None, int(numerator), int(denominator)
-
-    def __add__(self, other):
-        return whole, numerator1 + numerator2, denominator1
-
-
 def compute(equation):
     num1, operator, num2 = equation.split(' ')
+    num1, num2 = parse_num(num1), parse_num(num2)
+    answer = None
     if operator == '+':
-        whole, numerator, denominator = Fraction(parse_num(num1)) + Fraction(parse_num(num2))
-    return f"{numerator}/{denominator}"
+        answer = num1 + num2
+    elif operator == '*':
+        answer = num1 * num2
+    if answer is None:
+        return f"Invalid operator: {operator}"
+
+    return format_answer(answer)
 
 
 def parse_num(num):
-    """ Returns (whole, numerator, denominator)"""
+    if '/' not in num:
+        numerator, denominator = int(num), 1
+    elif '_' not in num:
+        numerator, denominator = num.split('/')
+        numerator, denominator = int(numerator), int(denominator)
+    else:
+        whole, fraction = num.split('_')
+        numerator, denominator = fraction.split('/')
+        whole, denominator = int(whole), int(denominator)
+        sign = -1 if whole < 0 else 1
+        # so that -1_2/3 is -1 + -1 * 2/3
+        numerator = int(whole) * denominator + sign * int(numerator)
+    return Fraction(numerator, denominator)
 
+
+def format_answer(answer):
+    if answer.denominator == 1:
+        return str(answer.numerator)
+    elif abs(answer.numerator) < answer.denominator:
+        return f"{answer.numerator}/{answer.denominator}"
+    elif abs(answer.numerator) > answer.denominator:
+        # -3//2 gives -2, so have to correct for that
+        sign = -1 if answer.numerator < 0 else 1
+        whole = sign * (abs(answer.numerator) // answer.denominator)
+
+        remainder = abs(answer.numerator) % answer.denominator
+        return f"{whole}_{remainder}/{answer.denominator}"
 
 
 if __name__ == '__main__':
--- tests.py
+++ tests.py
@@ -1,13 +1,59 @@
 import unittest
+from fractions import Fraction
+
 import main
+
+
+class TestParseNum(unittest.TestCase):
+    def test_one(self):
+        self.assertEqual(main.parse_num('1'), Fraction(1, 1))
+
+    def test_one_third(self):
+        self.assertEqual(main.parse_num('1/3'), Fraction(1, 3))
+
+    def test_two_thirds(self):
+        self.assertEqual(main.parse_num('2/3'), Fraction(2, 3))
+
+    def test_negative_one(self):
+        self.assertEqual(main.parse_num('-1'), Fraction(-1, 1))
+
+    def test_negative_one_third(self):
+        self.assertEqual(main.parse_num('-1/3'), Fraction(-1, 3))
+
+    def test_negative_one_and_two_thirds(self):
+        self.assertEqual(main.parse_num('-1_2/3'), Fraction(-5, 3))
+
+    def test_two_and_one_third(self):
+        self.assertEqual(main.parse_num('2_2/3'), Fraction(8, 3))
+
+    def test_bad_fraction(self):
+        self.assertRaises(ZeroDivisionError, main.parse_num, '1_1/0')
 
 
 class TestAddition(unittest.TestCase):
     def test_one_plus_one(self):
         self.assertEqual(main.compute('1 + 1'), '2')
 
-    def test_one_half_plus_one_half(self):
-        self.assertEqual(main.compute('1/2 + 1/2'), '1')
+    def test_one_third_plus_two_thirds(self):
+        self.assertEqual(main.compute('1/3 + 2/3'), '1')
+
+    def test_one_half_plus_one_third(self):
+        self.assertEqual(main.compute('1/2 + 1/3'), '5/6')
+
+    def test_one_half_plus_two_thirds(self):
+        self.assertEqual(main.compute('1/2 + 2/3'), '1_1/6')
+
+    def test_zero_plus_zero(self):
+        self.assertEqual(main.compute('0 + 0'), '0')
+
+    def test_negative_one_plus_one_third(self):
+        self.assertEqual(main.compute('-1 + 1/3'), '-2/3')
+
+    def test_negative_two_plus_one_third(self):
+        self.assertEqual(main.compute('-2 + 1/3'), '-1_2/3')
+
+    def test_two_and_three_eights_plus_nine_eights(self):
+        self.assertEqual(main.compute('2_3/8 + 9/8'), '3_1/2')
 
 
 if __name__ == '__main__':
