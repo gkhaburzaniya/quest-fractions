81e24f4174a527a54dc68e6041dccfb29ab6f136
George Khaburzaniya <george.khaburzaniya@gmail.com>
2022-04-13 11:33:06 -0700

    Multiplication.

--- tests.py
+++ tests.py
@@ -82,5 +82,34 @@ class TestSubtraction(unittest.TestCase):
         self.assertEqual(main.compute('2_3/8 - 9/8'), '1_1/4')
 
 
+class TestMultiplication(unittest.TestCase):
+    def test_one_times_one(self):
+        self.assertEqual(main.compute('1 * 1'), '1')
+
+    def test_one_third_times_two_thirds(self):
+        self.assertEqual(main.compute('1/3 * 2/3'), '2/9')
+
+    def test_one_half_times_one_third(self):
+        self.assertEqual(main.compute('1/2 * 1/3'), '1/6')
+
+    def test_one_half_times_two_thirds(self):
+        self.assertEqual(main.compute('1/2 * 2/3'), '1/3')
+
+    def test_zero_times_zero(self):
+        self.assertEqual(main.compute('0 * 0'), '0')
+
+    def test_one_times_zero(self):
+        self.assertEqual(main.compute('1 * 0'), '0')
+
+    def test_negative_one_times_one_third(self):
+        self.assertEqual(main.compute('-1 * 1/3'), '-1/3')
+
+    def test_negative_two_times_one_third(self):
+        self.assertEqual(main.compute('-2 * 1/3'), '-2/3')
+
+    def test_one_half_times_three_and_three_quarters(self):
+        self.assertEqual(main.compute('1/2 * 3_3/4'), '1_7/8')
+
+
 if __name__ == '__main__':
     unittest.main()
